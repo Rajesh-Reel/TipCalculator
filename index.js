4318d7a4d9d5e8338cbf1610415a261a43d4e5b2
Rajesh-Reel <rajreel2@hotmail.com>
2022-12-09 14:31:57 +0000

    calculator fn works and displays tip correctly

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ function calculateTip() {
         alert("Please enter values");
         return;
     }
+
+    let total = (billAmount * serviceQuality);
+    total = Math.round(total * 100) / 100;
+    total = total.toFixed(2)
+
+    document.getElementById("totalTip").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
 }
+
+//shows nothing on load
+document.getElementById("totalTip").style.display = "none";
